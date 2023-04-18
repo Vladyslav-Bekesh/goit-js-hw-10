e081102ae4bb05add698b1f8c984a08f5925f231
Vladyslav <dyadyavadya308@gmail.com>
2023-04-18 16:08:54 +0300

    task done
    aliluya

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const DEBOUNCE_DELAY = 300;
 
 const refs = {
   input: document.getElementById('search-box'),
-  countryList: document.querySelector('country-list'),
-  countryInfo: document.querySelector('country-info'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
 refs.input.addEventListener('input', _.debounce(onInput, DEBOUNCE_DELAY));
@@ -27,24 +27,25 @@ function onInput(event) {
   const countryName = refs.input.value;
   fetchCountries(countryName).then(countries => {
     checkAndCreateCountries(countries);
+  })
+  .catch(error => {
+    Notify.failure('Oops, there is no country with that name');
+    return;
   });
-  // .catch(error => {
-  //   Notify.failure('Oops, there is no country with that name');
-  //   return;
-  // });
 }
 
 function checkAndCreateCountries(countries) {
   if (countries.length === 1) {
-    createMarkup(
-      createForOneMatch(countries),
-      document.querySelector('country-info')
-    );
+    createMarkup(createForOneMatch(countries), refs.countryInfo);
+    refs.countryList.innerHTML = "";
+    console.log("deleted li");
+    return;
   } else if (countries.length <= 10 && countries.length > 1) {
-    createMarkup(
-      createForManyMathes(countries),
-      document.querySelector('country-list')
-    );
+    createMarkup(createForManyMathes(countries), refs.countryList);
+    refs.countryInfo.innerHTML = "";
+    console.log("deleted el");
+    return;
+
   } else {
     Notify.info('Too many matches found. Please enter a more specific name.');
   }
